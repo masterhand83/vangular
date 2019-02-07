b4862a894b0b9f684db8bdfab897dda2497e2a45
masterhand83 <robinmiguelsdj@gmail.com>
2019-02-07 10:25:23 -0600

    login y dashboard

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    this.renderer.removeClass(document.body,'bg-gradient-primary')
+  }
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body,'bg-gradient-primary');
+   }
+
+  ngOnInit() {
+  }
+
+}
