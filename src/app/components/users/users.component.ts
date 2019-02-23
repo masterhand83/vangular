099b4a9aa5bf3653e063cb05059c78a5d4c9095a
adminjacob <adjamoro@gmail.com>
2019-02-22 20:12:37 -0600

    agregar usuarios y tablas corregidas

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
+import {UsersService} from '../../services/users.service.1';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usersService:UsersService) { }
+  name:string;
+  lastname:string;
+  email:string;
+  mobile:string;
+  password:string;
+  userType:string
 
   ngOnInit() {
+  }
+
+  addUser(form: NgForm){
+    
+    
+
+    if(form.value.name!="" && form.value.lastname!="" && form.value.email!="" && form.value.mobile!=""
+      && form.value.password!="" && form.value.userType!=null &&
+      form.value.name!=undefined && form.value.lastname!=undefined && form.value.email!=undefined && form.value.mobile!=undefined
+      && form.value.password!=undefined && form.value.userType!=undefined 
+    
+    ){
+      
+      this.usersService.postUser(form.value)
+        .subscribe(res=>{
+          console.log(res); 
+            
+          form.reset();
+          alert("Usuario guardado exitosamente");
+          
+        });
+ 
+      
+    }
+    else{
+      alert('Favor de completar todos los campos');
+    } 
+
   }
 
 }
